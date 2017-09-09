28f95f4ccf9dbaee7e7b70fa07790f4a7c9d5f65
saliou <msalioubah37@gmail.com>
2017-09-09 03:11:45 +0200

    first commit

---
+++ src/main/java/com/example/demo/DemodeployementApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodeployementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodeployementApplication.class, args);
+	}
+}
